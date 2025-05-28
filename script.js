8771acb16c4ab419b64461c50f50a297519d9e66
mertgulexe <gul.mert89@gmail.com>
2025-05-28 16:18:53 +0100

    Add meshing function

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const mesh = document.querySelector(".mesh");
 const meshHeight = mesh.clientHeight;
 const meshWidth = mesh.clientWidth;
+const sizingButton = document.querySelectorAll(".sizing-button-container .sizing-button");
 
-function setMeshSize(xAxis=10, yAxis=20) {
+window.addEventListener("DOMContentLoaded", () => {
+    setMeshSize({
+        currentTarget: {
+            dataset: {
+                xAxis: 10,
+                yAxis: 20
+            }
+        }
+    })
+});
+
+sizingButton.forEach(
+    button => {
+        button.addEventListener("click", setMeshSize)
+    }
+);
+
+function setMeshSize(event) {
+    mesh.innerHTML = "";
+    const xAxis = parseInt(event.currentTarget.dataset.xAxis);
+    const yAxis = parseInt(event.currentTarget.dataset.yAxis);
     for (let i = 0; i < (xAxis * yAxis); i++) {
         const div = document.createElement("div");
         div.classList.add("mesh-content");
@@ -17,4 +38,3 @@ function setMeshSize(xAxis=10, yAxis=20) {
     );
 }
 
-setMeshSize();
