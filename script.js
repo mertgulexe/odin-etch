d57eef52c279c5eff2c2b1a12184c17424f7bef4
mertgulexe <gul.mert89@gmail.com>
2025-06-01 11:02:51 +0100

    Add hover effect to mesh blocks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const HOVER_CLASS_NAME = "hovered";
+const MESH_CONTENT_NAME = ".mesh-content";
 const mesh = document.querySelector(".mesh");
 const meshHeight = mesh.clientHeight;
 const meshWidth = mesh.clientWidth;
@@ -11,7 +13,8 @@ window.addEventListener("DOMContentLoaded", () => {
                 yAxis: 20
             }
         }
-    })
+    });
+    
 });
 
 sizingButton.forEach(
@@ -24,17 +27,30 @@ function setMeshSize(event) {
     mesh.innerHTML = "";
     const xAxis = parseInt(event.currentTarget.dataset.xAxis);
     const yAxis = parseInt(event.currentTarget.dataset.yAxis);
+    
     for (let i = 0; i < (xAxis * yAxis); i++) {
         const div = document.createElement("div");
         div.classList.add("mesh-content");
         mesh.appendChild(div);
     }
-    const meshContent = document.querySelectorAll(".mesh-content");
+    
+    const meshContent = document.querySelectorAll(MESH_CONTENT_NAME);
+    
     meshContent.forEach(
         div => {
             div.style.width = `${meshWidth / yAxis}px`;
             div.style.height = `${meshHeight / xAxis}px`;
         }
     );
+    
+    addHoverListeners(meshContent);
 }
 
+function addHoverListeners(element) {
+    element.forEach(
+        div => {
+            div.addEventListener("mouseover", e => e.currentTarget.classList.add(HOVER_CLASS_NAME));
+            div.addEventListener("mouseout", e => e.currentTarget.classList.remove(HOVER_CLASS_NAME));
+        }
+    );
+}
